981a34241cc3683e4b5b32033e50dbb723c9e885
imaeh24 <imaeh24@gmail.com>
2025-04-11 09:47:14 -0400

    day 5 of working on my web dev project. added the programs and values sections to the home page.

--- rw1/src/pages/home/home.jsx
+++ rw1/src/pages/home/home.jsx
@@ -1,10 +1,14 @@
 import Mainheader from "../../components/Mainheader"
+import Programs from '../../components/Programs'
+import Values from '../../components/Values'
 import "./home.css"
 
 const Home = () =>{
   return (
     <>
       <Mainheader/>
+      <Programs/>
+      <Values/>
     </>
   )
 }
